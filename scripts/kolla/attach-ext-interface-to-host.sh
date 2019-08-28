32eac8819d8fa1f8f527cdc92e5d40b398c1a125
Igor D.C <igordc.com@gmail.com>
2019-08-27 22:13:48 -0700

    Fix kolla ext interface script variables

--- scripts/kolla/attach-ext-interface-to-host.sh
+++ scripts/kolla/attach-ext-interface-to-host.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+EXT_INTF_IP=eno1 # edit
 sudo ip add del $EXT_INTF_IP dev eno1
 sudo docker exec -u 0 -it neutron_openvswitch_agent bash
-ovs-vsctl add-port br-ex eno1
+ovs-vsctl add-port br-ex $EXT_INTF_IP
 exit
--- scripts/kolla/attach-ext-interface-to-ovs.sh
+++ scripts/kolla/attach-ext-interface-to-ovs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+EXT_INTF_IP=eno1 # edit
 sudo docker exec -u 0 -it neutron_openvswitch_agent bash
-ovs-vsctl del-port br-ex eno1
+ovs-vsctl del-port br-ex $EXT_INTF_IP
 exit
 sudo netplan appy # bionic+
