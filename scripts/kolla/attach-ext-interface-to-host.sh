a2d2b453caf0fd09c6b71fee4f727452e585fc52
Igor D.C <igordc.com@gmail.com>
2019-08-29 14:40:05 -0700

    Correct/improve attach ext interface scripts

--- scripts/kolla/attach-ext-interface-to-host.sh
+++ scripts/kolla/attach-ext-interface-to-host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EXT_INTF_IP=eno1 # edit
-sudo ip add del $EXT_INTF_IP dev eno1
-sudo docker exec -u 0 -it neutron_openvswitch_agent bash
-ovs-vsctl add-port br-ex $EXT_INTF_IP
+EXT_IF=eno1 # edit
+sudo docker exec -u 0 -it neutron_openvswitch_agent env EXT_IF=$EXT_IF bash
+ovs-vsctl del-port br-ex $EXT_IF
 exit
+sudo netplan apply # bionic+
--- scripts/kolla/attach-ext-interface-to-ovs.sh
+++ scripts/kolla/attach-ext-interface-to-ovs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-EXT_INTF_IP=eno1 # edit
-sudo docker exec -u 0 -it neutron_openvswitch_agent bash
-ovs-vsctl del-port br-ex $EXT_INTF_IP
+EXT_IF=eno1 # edit
+EXT_IP=$(ip a show dev $EXT_IF | grep "inet " | grep -E -o  [0-9]+.[0-9]+.[0-9]+.[0-9]+/[0-9]+)
+sudo ip add del $EXT_IP dev $EXT_IF
+sudo docker exec -u 0 -it neutron_openvswitch_agent env EXT_IF=$EXT_IF bash
+ovs-vsctl add-port br-ex $EXT_IF
 exit
-sudo netplan appy # bionic+
