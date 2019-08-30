e3923fc08ae47630db0038d0aaf0ba78a5efebaa
Igor D.C <igordc.com@gmail.com>
2019-08-30 12:02:40 -0700

    Improve chameleonsocks-auto-proxy.sh

--- scripts/redsocks/chameleonsocks-auto-proxy.sh
+++ scripts/redsocks/chameleonsocks-auto-proxy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-### igordc, 20190622,   ###
-###         20190719    ###
-### run as root  user   ###
-### ran fine on 16.04.6 ###
-### ran fine on 18.04.2 ###
-### TODO use containerd ###
+## igordc, 20190622, 20190719, 20190830
+## run as root
+## ran fine on 16.04.6, 18.04.2
+## TODO use containerd
 
 #sudo su -
 cd
 
-# CHANGE THIS:
+# edit:
 HTTP_PROXY=http://server.localhost:port
 HTTPS_PROXY=http://server.localhost:port
-SOCKS_PROXY=server.localhost # assumes port 1080
+SOCKS_PROXY=server.localhost
+SOCKS_PORT=1080
 
 # temporary proxy settings
 export HTTP_PROXY
@@ -34,25 +33,11 @@ apt-get update
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 apt-get install docker-ce docker-ce-cli containerd.io build-essential -y
 
-wget https://raw.githubusercontent.com/crops/chameleonsocks/master/chameleonsocks.sh
+wget https://raw.githubusercontent.com/igordcard/chameleonsocks/master/chameleonsocks.sh
+wget https://raw.githubusercontent.com/igordcard/chameleonsocks/master/confs/chameleonsocks.exceptions
 sed -i "s/PROXY:=my.proxy.com/PROXY:=$SOCKS_PROXY/" chameleonsocks.sh
-sed -i "s/EXCEPTIONS:=\/path\/to\/exceptions\/file/EXCEPTIONS:=chameleonexceptions/" chameleonsocks.sh
-
-touch chameleonexceptions
-tee chameleonexceptions << EOF
-0.0.0.0/8
-127.0.0.0/8
-169.254.0.0/16
-224.0.0.0/4
-240.0.0.0/4
-10.0.0.0/8
-172.16.0.0/12
-192.168.0.0/16
-10.178.241.0/24
-172.25.103.0/24
-172.17.0.0/16
-192.168.122.0/24
-EOF
+sed -i "s/PORT:=1080/PORT:=$SOCKS_PORT/" chameleonsocks.sh
+sed -i "s/EXCEPTIONS:=\/path\/to\/exceptions\/file/EXCEPTIONS:=chameleonsocks.exceptions/" chameleonsocks.sh
 
 chmod +x chameleonsocks.sh
 ./chameleonsocks.sh --install
